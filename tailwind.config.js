c46e37b435ad8c145626feec1d622db53a4d62a5
aimdexter <draimade@gmail.com>
2021-10-01 15:19:31 +0000

    desktop preview header and 1st section done note responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        fuchsia: colors.fuchsia,
+      },
       listStyleType: {
         square: 'square',
         roman: 'upper-roman',
