e7e39042e987546c400a65546f7cd2dec2d670bd
aimdexter <draimade@gmail.com>
2021-10-01 10:36:49 +0000

    assets and text added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,9 @@ module.exports = {
         square: 'square',
         roman: 'upper-roman',
       },
-      colors: {
-        emplois: 'hsl(180, 62%, 55%)',
-        ad: 'hsl(0, 78%, 62%)',
-        rh: 'hsl(34, 97%, 64%)',
-        secm: 'hsl(212, 86%, 64%)',
-        infos: '#083102',
-      },
       fontFamily: {
         'fa': ['Poppins'],
-      },
-      boxShadow: {
-        fa: '0 8px 16px 0 rgba(0, 0, 0, 0.2)',
-        emplois: '0 8px 16px 0 hsl(180, 62%, 55%)',
-        ad: '0 8px 16px 0 hsl(0, 78%, 62%)',
-        rh: '0 8px 16px 0 hsl(34, 97%, 64%)',
-        secm: '0 8px 16px 0 hsl(212, 86%, 64%)',
-        infos: '0 8px 16px 0 #083102',
+        'Montserrat':['Montserrat', 'sans-serif'],
       }
     },
   },
